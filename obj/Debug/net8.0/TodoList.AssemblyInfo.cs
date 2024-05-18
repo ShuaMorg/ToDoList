806aa0c319e8d9c2bcd91e863c8788ac98a5552f
ShuaMorg <joshmorganjones@outlook.com>
2024-05-18 22:48:21 +0100

    test2

--- obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf524ff3010cfdcd1e7966a2e6809a542308319")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9a40e45b60f1843f5af3619c1830ea1f75343d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
